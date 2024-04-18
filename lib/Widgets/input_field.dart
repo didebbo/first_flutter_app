924875a1c6e7902ff359fd158ce5c2d66bb24efa
Gianluca Napoletano <gi.napoletano@reply.it>
2024-04-18 18:20:59 +0200

    handle input text with confirm button

--- lib/Widgets/input_field.dart
+++ lib/Widgets/input_field.dart
@@ -12,8 +12,8 @@ class InputField extends StatefulWidget {
 }
 
 class _InputField extends State<InputField> {
-  late String name;
-  late String surname;
+  String name = "";
+  String surname = "";
 
   @override
   Widget build(BuildContext context) {
@@ -26,36 +26,38 @@ class _InputField extends State<InputField> {
           padding: const EdgeInsets.all(20),
           color: widget.backgroundColor,
           child: Row(children: [
-            inputField("Nome"),
-            inputField("Cognome"),
+            inputField("Nome", (val) => name = val),
+            inputField("Cognome", (val) => surname = val),
             confirmButton()
           ]),
         ));
   }
 
-  Widget inputField(String label) {
+  Widget inputField(String label, Function(String) onChanged) {
     return Expanded(
         child: Container(
             margin: const EdgeInsets.symmetric(vertical: 0, horizontal: 10),
             child: TextField(
-                decoration: InputDecoration(
-                    hoverColor: Colors.yellow.withAlpha(50),
-                    labelText: label,
-                    labelStyle: TextStyle(
-                      color: Colors.black.withAlpha(100),
-                    ),
-                    filled: true,
-                    enabledBorder: OutlineInputBorder(
-                        borderSide:
-                            BorderSide(color: Colors.pink.shade200, width: 1),
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(10))),
-                    focusedBorder: OutlineInputBorder(
-                        borderSide:
-                            BorderSide(color: Colors.pink.shade400, width: 1.2),
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(10))),
-                    fillColor: Colors.white))));
+              decoration: InputDecoration(
+                  hoverColor: Colors.yellow.withAlpha(50),
+                  labelText: label,
+                  labelStyle: TextStyle(
+                    color: Colors.black.withAlpha(100),
+                  ),
+                  filled: true,
+                  enabledBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Colors.pink.shade200, width: 1),
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(10))),
+                  focusedBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Colors.pink.shade400, width: 1.2),
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(10))),
+                  fillColor: Colors.white),
+              onChanged: onChanged,
+            )));
   }
 
   Widget confirmButton() {
@@ -64,7 +66,7 @@ class _InputField extends State<InputField> {
             color: Colors.pink,
             borderRadius: BorderRadius.all(Radius.circular(10))),
         child: IconButton(
-            onPressed: () => {},
+            onPressed: () => {print("Name is $name $surname")},
             icon: Icon(color: Colors.pink[50], Icons.send)));
   }
 }
